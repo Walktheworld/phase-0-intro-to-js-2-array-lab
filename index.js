83a55db4055037ab830f5b6cf09d5c7ec3ce89bd
Joe Brown <walktheworld@me.com>
2021-03-25 20:48:50 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield']
+
+
+function destructivelyAppendCat (Ralph){
+    cats.push("Ralph");
+    return cats;
+}
+
+
+function destructivelyPrependCat (Bob){
+    cats.unshift('Bob');
+    return cats;
+}
+
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+
+
+const copyOfCats = [...cats]
+
+function appendCat(Broom){
+    const copyOfCats = [...cats, 'Broom']
+    return copyOfCats
+}
+
+
+function prependCat(Arnold){
+    const copyOfCats = [ 'Arnold', ...cats]
+    return copyOfCats
+}
+
+
+function removeLastCat(){
+    const copyOfCats = cats.slice(0, cats.length - 1)
+    return copyOfCats
+}
+
+
+function removeFirstCat(){
+    const copyOfCats = cats.slice(1)
+    return copyOfCats
+}
+
+
